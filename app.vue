2f633ab6323e39e2fc928095e30eace41a3ae428
Steven John <stevenjpx2@gmail.com>
2022-01-03 11:26:43 +0400

    first commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="grid place-items-center h-screen font-mono text-3xl text-white bg-slate-900"
+  >
+    Hello
+  </div>
+</template>
+
+<style lang="scss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
